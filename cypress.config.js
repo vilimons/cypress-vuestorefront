e949d708aecb05374a1c89c88c4ae564eeb9914e
unknown <limavibrandao@gmail.com>
2023-09-26 17:41:39 -0300

    reports implemented

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,21 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: true,
+    charts: true,
+    embeddedScreenshots: true,
+    saveAllAttempts: true,
+    html: true,
+    json: false,
+  },
   chromeWebSecurity: false,
   e2e: {
     baseUrl: "https://demo.vuestorefront.io/",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on)
     },
   },
 });
